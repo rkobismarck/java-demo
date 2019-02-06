3101e919ae4de13b411de94e1963f225c1ff8b4f
Roberto Trujillo <robertotrujillo@MacBook-Pro-de-Roberto-2.local>
2019-02-06 12:29:12 -0600

    Added sh

---
+++ example/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+FILENAME="pid.txt"
+	while read -r line; do
+		PID="$line"
+	done < "$FILENAME"
+	if ps -p "$PID" < /dev/null
+		then
+   		string=$(ps -p "$PID" -o comm=) 
+   		if [[ $string == *"java"* ]]; 
+   			then
+  				echo "Instance running actually."
+  			else
+  				echo "Starting new instance:"
+  				nohup java -jar Example.jar 100000 >log.out 2>&1 & echo $! > pid.txt
+		fi
+		else
+				echo "Starting new instance:"
+  				nohup java -jar Example.jar 100000 >log.out 2>&1 & echo $! > pid.txt
+			
+	fi
+	
