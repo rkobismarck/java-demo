1f26ebf6182766dc82e7c22c0af9339cc2cb412f
Roberto Carlos Trujillo Torres <rko.bismarck@gmail.com>
2019-02-06 14:55:52 -0600

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -11,11 +11,11 @@ FILENAME="pid.txt"
   				echo "Instance running actually."
   			else
   				echo "Starting new instance:"
-  				nohup java -jar Example.jar 300 >log.out 2>&1 & echo $! > pid.txt
+  				nohup java -jar helloWorld-0.0.1-SNAPSHOT.jar 300 >log.out 2>&1 & echo $! > pid.txt
 		fi
 		else
 				echo "Starting new instance:"
-  				nohup java -jar Example.jar 300 >log.out 2>&1 & echo $! > pid.txt
+  				nohup java -jar helloWorld-0.0.1-SNAPSHOT.jar 300 >log.out 2>&1 & echo $! > pid.txt
 			
 	fi
 	
